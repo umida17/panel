4034f8d4678f2b6ccf365443214d0396ae41bb48
uy <a.normurzaev20@gmail.com>
2026-05-21 14:00:58 +0500

    sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,26 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import { LanguageProvider } from './LanguageContext' 
+import { LanguageProvider } from "./LanguageContext";
 import Contact from './Pages/ContactPages/ContactPages';
 import Students from './Pages/StudentPages/StudentPages';
 import Teachers from './Pages/TeachersPages/TeachersPages';
 import Layout from './Companent/Layout/Layout';
-import { LanguageContex } from './LanguageContext/LanguageContext';
 
 const App = () => {
   return (
-    <LanguageContex>  
+    <LanguageProvider>
       <BrowserRouter>
         <Routes>
+          
           <Route element={<Layout />}>
             <Route path='/Contact' element={<Contact />} />
             <Route path='/Students' element={<Students />} />
             <Route path='/Teachers' element={<Teachers />} />
           </Route>
+         
         </Routes>
       </BrowserRouter>
-    </LanguageContex>
+    </LanguageProvider>
   )
 }
 
