f35bfc8f9494794ce636c2d22900df2b45d9d539
uy <github.com/umida17>
2026-05-21 13:17:55 +0500

    contact

--- src/Pages/TeachersPages/TeachersPages.jsx
+++ src/Pages/TeachersPages/TeachersPages.jsx
@@ -1,11 +1,42 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useOutletContext } from 'react-router-dom'  
+import './TeachersPages.css'  
 
 const TeachersPages = () => {
+  const [teachers, setTeachers] = useState([])
+  
+   const { searchQuery } = useOutletContext()
+
+  useEffect(() => {
+     fetch("https://699d9b4283e60a406a46e1ba.mockapi.io/Teachers")
+      .then(response => response.json())
+      .then(data => setTeachers(data))
+      .catch(err => console.log("Xatolik:", err))
+  }, [])
+
+   const filteredTeachers = teachers.filter(teacher => 
+    teacher.name.toLowerCase().includes(searchQuery.toLowerCase())
+  )
+
   return (
-    <div>
-      <h1>Teachers Pages</h1>
-      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsum dolore ipsa incidunt.
-      </p>
+    <div className="teachers-page-container">
+      <div className="teachers-grid">
+        
+        {filteredTeachers.length > 0 ? (
+          filteredTeachers.map((el) => (
+            <div key={el.id} className="teacher-card">
+              <img src={el.avatar} alt={el.name} className="teacher-avatar" />
+              <h3 className="teacher-name">{el.name}</h3>
+              <button className="teacher-btn">Profile View</button>
+            </div>
+          ))
+        ) : (
+           <div style={{ gridColumn: '1 / -1', textAlign: 'center', padding: '40px', color: '#888' }}>
+            Bunday o'qituvchi topilmadi...
+          </div>
+        )}
+
+      </div>
     </div>
   )
 }
