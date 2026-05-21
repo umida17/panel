7f608c37ab96740220aacda58e92f700be1c37a2
uy <a.normurzaev20@gmail.com>
2026-05-21 13:51:03 +0500

    sidebar

--- src/Companent/Sidebar/Sidebar.jsx
+++ src/Companent/Sidebar/Sidebar.jsx
@@ -1,62 +1,32 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { useLang } from './LanguageContext' // Import qilamiz
 
 const Sidebar = () => {
+  const { t } = useLang(); // t funksiyasini olamiz
+
   return (
-     <div className='sidebar' style={{ padding: '20px', height: '100%' }}>
+    <div className='sidebar' style={{ padding: '20px', height: '100%' }}>
       <div className='sidebar-cantainer'>
-
-         <h1 className='logo' style={{ 
-          fontSize: '64px', 
-          fontWeight: 'bold', 
-          marginBottom: '30px',
-          color: '#ec38da',
-          textAlign: 'center'
-        }}>
-          Logo
+        <h1 className='logo' style={{ fontSize: '24px', color: '#fff', marginBottom: '30px', textAlign: 'center' }}>
+          {t('logo')}
         </h1>
-
-         <ul style={{ listStyle: 'none', padding: 0, margin: 0 }}>
-          
+        <ul style={{ listStyle: 'none', padding: 0 }}>
           <li style={{ marginBottom: '15px' }}>
-             <Link to="/Contact" style={{ 
-              color: '#a3a6b7', 
-              textDecoration: 'none',
-              fontSize: '16px',
-              display: 'block',
-              padding: '10px',
-              borderRadius: '6px'
-            }}>
-              Contact Pages
+            <Link to="/Contact" style={{ color: '#a3a6b7', textDecoration: 'none' }}>
+              {t('contactPages')}
             </Link>
           </li>
-
           <li style={{ marginBottom: '15px' }}>
-             <Link to="/Students" style={{ 
-              color: '#a3a6b7', 
-              textDecoration: 'none',
-              fontSize: '16px',
-              display: 'block',
-              padding: '10px',
-              borderRadius: '6px'
-            }}>
-              Students Pages
+            <Link to="/Students" style={{ color: '#a3a6b7', textDecoration: 'none' }}>
+              {t('studentsPages')}
             </Link>
           </li>
-
           <li style={{ marginBottom: '15px' }}>
-             <Link to="/Teachers" style={{ 
-              color: '#a3a6b7', 
-              textDecoration: 'none',
-              fontSize: '16px',
-              display: 'block',
-              padding: '10px',
-              borderRadius: '6px'
-            }}>
-              Teachers Pages
+            <Link to="/Teachers" style={{ color: '#a3a6b7', textDecoration: 'none' }}>
+              {t('teachersPages')}
             </Link>
           </li>
-
         </ul>
       </div>
     </div>
